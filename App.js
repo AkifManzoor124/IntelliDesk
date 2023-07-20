325db00089c30c9362f5388dc3f22bda9aed65ea
Akif <akifmanzoor124@gmail.com>
2023-07-20 19:52:27 -0400

    Created tab navigator and template pages

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import Authentication from './Components/Authentication';
+import Home from './Components/Home';
+import RGBControl from './Components/RGBControl';
+import Metrics from './Components/Metrics';
+import Settings from './Components/Settings';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen name="Home" component={Home} />
+          <Tab.Screen name="RGBControl" component={RGBControl} />
+          <Tab.Screen name="Metrics" component={Metrics} />
+          <Tab.Screen name="Settings" component={Settings} />
+        </Tab.Navigator>
+      </NavigationContainer>
     </View>
   );
 }
