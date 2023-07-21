38deaf088fc06fff1a456347fb272ca30bf6cbea
Akif <akifmanzoor124@gmail.com>
2023-07-20 20:44:15 -0400

    Created tab navigator & template pages, fixed bugs

--- Components/RGBControl.js
+++ Components/RGBControl.js
@@ -1,9 +1,9 @@
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function Home() {
+export default function RGBControl() {
   return (
     <View style={styles.container}>
-        <Text>Home</Text>
+        <Text>RGBControl</Text>
     </View>
   );
 }
