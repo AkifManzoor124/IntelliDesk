7b6ccdb1ed21c5e7e1cbfbd7fcee1010f03634e5
Akif <akifmanzoor124@gmail.com>
2023-09-07 21:27:50 -0400

    created the skeleton for the home page

--- Components/Home.js
+++ Components/Home.js
@@ -13,28 +13,46 @@ export default function Home() {
   }
 
   return (
+
     <View style={styles.container}>
-
-      <View style={{backgroundColor: 'blue', flex: 0.3}} />
-      <View style={{backgroundColor: 'red', flex: 0.3, borderBottomRightRadius: 50}} >
-
-        <Slider
-          style={{width: 200, height: 40}}
-          minimumValue={0}
-          maximumValue={1}
-          minimumTrackTintColor="#FFFFFF"
-          maximumTrackTintColor="#000000"
-          onValueChange={slideHandler}
-        />
-        
-        <Text> {pos} </Text>
-        
-        {/* create a standing desk with shapes and move it with a slide bar */}
-
+      <View style={styles.header} />
+        <Text> Good Morning, Kumberly Janiqua </Text>
+        <Text> Notification Icon </Text>
+      <View style={styles.quickMetrics} />
+        <Text> Metrics </Text>
+        <View style={styles.metrics} >
+          <Text> Standing </Text>
+          <Text> Sitting </Text>
+          <Text> Trend </Text>
+        </View>
+      <View style={styles.tableControl} >
+        <View style={styles.table} >
+          <Text> Bluetooth Icon </Text>
+          <Text> Kumberly's Standing Desk </Text>
+        </View>
+        <View style={styles.slider}>
+          <View style={{backgroundColor: 'red', flex: 0.3, borderBottomRightRadius: 50}} >
+              <Slider
+                style={{width: 200, height: 40}}
+                minimumValue={0}
+                maximumValue={1}
+                minimumTrackTintColor="#FFFFFF"
+                maximumTrackTintColor="#000000"
+                onValueChange={slideHandler}
+              />
+              <Text> {pos} </Text>
+              {/* create a standing desk with shapes and move it with a slide bar */}
+          </View>
+          <View style={[styles.circle, {top: -150 - pos*100}]} />
+        </View>
+        <View style={styles.tableButtons} >
+          <Text> P1 </Text>
+          <Text> P2 </Text>
+          <Text> P3 </Text>
+          <Text> P4 </Text>
+          <Text> P5 </Text>
+        </View>
       </View>
-
-      <View style={[styles.circle, {top: -150 - pos*100}]} />
-
     </View>
   );
 }
@@ -44,24 +62,35 @@ const windowWidth = Dimensions.get('window').width;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "grey",
-    // alignItems: 'center',
-    // justifyContent: 'center',
+    backgroundColor: "white",
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  // circle: {
-  //   top: -50,
-  //   left: windowWidth-50,
-  //   top: -75,
-  //   width: 100,
-  //   height: 100,
-  //   borderRadius: 100 / 2,
-  //   backgroundColor: "green",
-  // },
+  header: {
+    backgroundColor: 'red',
+    flexDirection: 'row',
+  },
+  quickMetrics: {
+    backgroundColor: 'orange',
+  },
+  table: {
+    backgroundColor: 'blue', 
+    flex: 0.3
+  },
+  metrics: {
+    flexDirection: 'row',
+    backgroundColor: 'yellow',
+  },
   circle: {
+    position: "relative",
     left: windowWidth/2,
-    width: 100,
-    height: 100,
+    width: 20,
+    height: 20,
     borderRadius: 100 / 2,
     backgroundColor: "pink",
   },
+  tableButtons: {
+    flexDirection: 'row',
+    backgroundColor: 'green',
+  }
 });
